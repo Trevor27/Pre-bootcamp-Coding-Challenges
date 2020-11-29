eeae2c7a357b58b02bafdc046722d76b84357fb4
Trevor27 <56981147+Trevor27@users.noreply.github.com>
2020-11-29 21:04:44 +0200

    Task 6 added

---
+++ Task6.py
@@ -0,0 +1,16 @@
+def max_num():
+
+    x = input("Enter a number")
+    y = input("Enter a number") 
+    z = input("Enter a number")
+
+    if x > y and z:
+        print(x)
+
+    elif y > x and z:
+        print(y)
+
+    else:
+        print(z)
+
+max_num()
