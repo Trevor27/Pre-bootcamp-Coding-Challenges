e68524b35e51a2f3e6847bcb7ef1c87713c9c1b2
Mpho Trevor Mashau <56981147+Trevor27@users.noreply.github.com>
2020-11-30 15:33:27 +0200

    Update Task10.py

--- Task10.py
+++ Task10.py
@@ -1,11 +1,9 @@
 def vowels():
 
-    myString = str(input("Enter a string: "))
-
     myString.lower()
     
     for vowel in myString:
         if vowel in 'aeiou':
             print(vowel)
 
-vowels()
+vowels("umuzi")
