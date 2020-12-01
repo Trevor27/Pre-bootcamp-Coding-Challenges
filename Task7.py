2432e2beb6ca20514802251c8aeceff6fdfcb651
Mpho Trevor Mashau <56981147+Trevor27@users.noreply.github.com>
2020-12-01 12:17:34 +0200

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -1,16 +1,9 @@
 def tempC_Fahrenheit(temp):
-
     fahrenheit = temp * (1.8) + 32
-
     print("Temperature in Fahrenheit is: " + str(fahrenheit))
-
 tempC_Fahrenheit(25)
 
-
 def tempF_Celcius(temp):
-
     celcius = (temp - 32) / 1.8
-
     print("Temperature in Celcius is: " + str(celcius))
-
 tempF_Celcius(77)
