a4b253819b62bb7988c33072974a778df049c159
Mpho Trevor Mashau <56981147+Trevor27@users.noreply.github.com>
2020-12-01 12:02:38 +0200

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,12 +1,7 @@
 
 def area_of_triangle(a,b,c):
-
     s = (1/2)*(a + b + c)
-  
     z = s*(s - a)*(s - b)*(s - c)
-
     area = (z)**(1/2)
-    
-    print("The area of the triangle is: "+ str(area))
-
-area_of_triangle(4,5,6)
+    return area
+print(area_of_triangle(4,5,6))
