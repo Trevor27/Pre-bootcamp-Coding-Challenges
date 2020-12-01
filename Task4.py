c66d748589a160a5c3221993c56d138aa33858d3
Mpho Trevor Mashau <56981147+Trevor27@users.noreply.github.com>
2020-12-01 11:51:07 +0200

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -2,7 +2,7 @@ def my_function(x,y):
 
     mySum = x + y    
 
-    if x == 3 or y == 3 and "3" in str(mySum) :
+    if x == 3 or (y == 3 and "3" in str(mySum)) :
         return True
     
     else:
