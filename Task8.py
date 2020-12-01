6f42c481e39fd80fb283ee6aefb3944b74e372b9
Mpho Trevor Mashau <56981147+Trevor27@users.noreply.github.com>
2020-12-01 12:18:55 +0200

    Update Task8.py

--- Task8.py
+++ Task8.py
@@ -1,15 +1,9 @@
 def convert_time(time):
-    
     hour = time // 60
-
     time %= 60
-
     minutes = time
-
     if hour <= 1:
         print(str(hour) + " hour, " + str(minutes) + (" minutes"))
-
     else:
         print(str(hour) + " hours, " + str(minutes) + (" minutes"))
-
 convert_time(122)
