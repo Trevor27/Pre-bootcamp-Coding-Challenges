a3ddda8e29437501fb7bbfab1b99aa32932f9c51
Mpho Trevor Mashau <56981147+Trevor27@users.noreply.github.com>
2020-12-07 21:00:45 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,6 +1,6 @@
-def my_function(x,y):
+def compare_num(x,y):
     if x == 65 or y == 65 or (x + y) == 65:
         return True
     else:
         return False
-print(my_function(65,25))
+print(compare_num(65,25))
